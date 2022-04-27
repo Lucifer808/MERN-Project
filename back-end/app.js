a00de8790f6a9cd86ffd186b40653d03124e6a45
Lucifer808 <autochess1235@gmail.com>
2022-04-27 11:35:10 +0700

    back end

--- back-end/app.js
+++ back-end/app.js
@@ -4,6 +4,9 @@ const errorMiddleware = require("./middleware/error");
 const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const fileUpload = require("express-fileupload");
+const dotenv = require("dotenv");
+// config
+dotenv.config({ path: "back-end/config/config.env" });
 app.use(express.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,6 +20,9 @@ app.use("/api/v1", user);
 // Route Order
 const order = require("./routes/orderRoute");
 app.use("/api/v1", order);
+// Route Payment
+const payment = require("./routes/paymentRoute");
+app.use("/api/v1", payment);
 // Middleware handler error
 app.use(errorMiddleware);
 module.exports = app;
