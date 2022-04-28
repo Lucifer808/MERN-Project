a803b2b1d4e4d96730735d726c1b5a594353a1c5
Lucifer808 <autochess1235@gmail.com>
2022-04-28 19:29:45 +0700

    back-end

--- back-end/models/orderModel.js
+++ back-end/models/orderModel.js
@@ -15,6 +15,10 @@ const orderSchema = new Schema({
             type: String,
             required: true,
         },
+        state: {
+            type: String,
+            required: true,
+        },
         pinCode: {
             type: Number,
             required: true,
